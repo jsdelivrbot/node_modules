56f16e4def43bf6f0ed44e5578f62f7876bf13fe
Bing Xu <bxu@pivotal.io>
2017-06-08 16:24:36 +0800

    clean again to revert to nodejs 6.2.2

--- pui-css-dropdowns/index.js
+++
@@ -1,8 +0,0 @@
-try {
-  require('pui-css-bootstrap');
-  require('pui-css-buttons');
-  require('pui-css-button-group');
-  require('pui-css-links');
-  require('./dropdowns.css');
-} catch(e) {
-}
